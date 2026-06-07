d10045413046b0be80e44209158b4ff30fe1caee
Brooke Pillifant <brookepillifant@Brookes-MacBook-Air.local>
2026-06-07 12:14:09 -0400

    things change

--- WorldCupTraffic/src/toy_examples.jl
+++ WorldCupTraffic/src/toy_examples.jl
@@ -1,5 +1,6 @@
 using DifferentialEquations
 using Plots
+using Distributions
 
 function coupled_transport!(dP, P, params, t)
     ns, dxs, velocities, boundary_conds, attractions = params
@@ -145,4 +146,55 @@ function TwoNodeAnim(sol, ns, Ls; max_frames=300)
     return anim
 end
 
-export TwoNodeSystem, TwoNodeAnim, TwoNodePlot
+
+
+function SoccerGame(t1)
+    time = 0;
+    score = [0 0];
+
+    # probabilities of each team scoring
+    t2 = 1-t1;
+
+    # the main game
+    while time < 90
+        time += rand(Exponential(lambda));
+        if time < 90
+            g = rand(Uniform(0,1))
+            if g < t1
+                score[1] += 1
+            else
+                score[2] += 1
+            end
+        end
+    end
+    #overtime
+    if score[1] == score[2]
+        while time < 120
+        time += rand(Exponential(lambda));
+            if time < 120
+                g = rand(Uniform(0,1))
+                if g < t1
+                    score[1] += 1
+                else
+                    score[2] += 1
+                end
+            end
+        end
+    end
+    #penalty shootout
+    if score[1] == score[2]
+        while score[1] == score[2]
+            gt1 = rand(Uniform(0,1))
+            gt2 = rand(Uniform(0,1))
+            if gt1 < t1
+                score[1] += 1
+            end
+            if gt2 < t2
+                score[2] += 1
+            end
+        end
+    end
+    return score
+end
+
+export TwoNodeSystem, TwoNodeAnim, TwoNodePlot, SoccerGame
